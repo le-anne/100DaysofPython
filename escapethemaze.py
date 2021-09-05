907e5e7222eae614e980f30a3e97fc62f856dfaa
Leanne <76232496+le-anne@users.noreply.github.com>
2021-09-05 08:03:11 -0700

    created escape the maze file

---
+++ escapethemaze.py
@@ -0,0 +1,15 @@
+move()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
